d1592c64b42810f23342ea85d1fdd6096d9479ba
Andrea Valdecantos <andreavaldecantostorres@gmail.com>
2023-06-21 12:33:08 -0300

    lista de tareas pendientes

---
+++ practico8/ClaseTarea.cs
@@ -0,0 +1,8 @@
+namespace ListaDeTareas{
+
+    public class Tareas{
+        public int TareaID {get; set;}
+        public string? Descripcion {get; set;}
+        public int Duracion {get; set;}
+    }
+}
---
+++ practico8/Program.cs
@@ -0,0 +1,24 @@
+﻿using ListaDeTareas;
+
+List<Tareas> TareasPendientes = new List<Tareas>();
+List<Tareas> TareasRealizadas = new List<Tareas>();
+string? descripcion;
+
+for (int i = 0; i < 5; i++)
+{
+    Tareas Tarea = new Tareas();
+    var numeroRandom = new Random();
+    Console.WriteLine($"Ingrese la descripción de la tarea {i+1}: ");
+    descripcion = Console.ReadLine();
+    Tarea.Descripcion = descripcion;
+    Tarea.TareaID = i+1;
+    Tarea.Duracion = numeroRandom.Next(10,100);
+    TareasPendientes.Add(Tarea);
+    
+}
+
+Console.WriteLine("\n------------------------\nLISTA DE TAREAS PENDIENTES");
+foreach (var tarea in TareasPendientes)
+{
+    Console.WriteLine($"Tarea {tarea.TareaID}\nDescripción: {tarea.Descripcion}\nDuración: {tarea.Duracion}\n");
+}
